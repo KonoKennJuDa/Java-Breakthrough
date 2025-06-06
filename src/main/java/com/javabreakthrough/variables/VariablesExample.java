c7b0cce9fb7cad5d5ac390fc140f43830db0287f
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-06 21:59:00 +0300

    Completed lesson №1 Commit №2

---
+++ src/main/java/com/javabreakthrough/variables/VariablesExample.java
@@ -0,0 +1,20 @@
+package com.javabreakthrough.variables;
+
+public class VariablesExample {
+
+    public static void main(String[] args) {
+        // Примитивные типы
+        int age = 25;
+        double weight = 65.5;
+        boolean isStudent = true;
+        double temperature = -5.3;
+
+        // Ссылочные типы
+        String name = "Мария";
+        int[] grades = {5, 4, 3};
+
+        System.out.println(name + ", возраст: " + age + ", температура тела: " + temperature);
+        System.out.println("Вес: " + weight + " кг");
+        System.out.println("Студент: " + isStudent);
+    }
+}
