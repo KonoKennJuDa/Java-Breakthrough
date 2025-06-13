98c12b1b3d6e2b6c525cd207178f6e5c50749a6c
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-13 23:51:18 +0300

    Commit №11

---
+++ src/main/java/com/javabreakthrough/Lesson2/Operators/Switch.java
@@ -0,0 +1,28 @@
+package com.javabreakthrough.Lesson2.Operators;
+
+import java.util.Scanner;
+
+public class Switch {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ведите возраст");
+        int age = scanner.nextInt();
+        switch (age) {
+            case 0 :
+                System.out.println("Ты родился");
+                break;
+            case 7 :
+                System.out.println("Ты пошел в школу");
+                break;
+            case 18 :
+                System.out.println("Ты закончил школу");
+                break;
+            default:
+                System.out.println("Ни одно из предыдущих условий не подошло");
+        }
+
+        // Также с Java 7 мы также можем пользоваться и String в switch
+    }
+}
