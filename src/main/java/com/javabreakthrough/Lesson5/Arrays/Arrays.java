8cabff3c6b1f5413a026fe74e400a7706edb4f86
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-14 00:17:07 +0300

    Commit №11а

--- src/main/java/com/javabreakthrough/Lesson5/Arrays/Arrays.java
+++ src/main/java/com/javabreakthrough/Lesson5/Arrays/Arrays.java
@@ -4,18 +4,21 @@ public class Arrays {
 
     public static void main(String[] args) {
 
-        int number = 10;
-        int[] numbers = new int[5];
-        for (int i = 0; i < numbers.length; i++) {
+        int number = 10; // Примитивный тип данных
+        char character = 'A'; // Примитивный тип данных
+        String string = "Hello"; // Ссылочный тип данных. Уже писал, что String это класс. Из класса создается объект
+
+        int[] numbers = new int[5]; // numbers ==> Массив int. Ссылочный тип данных. Пока что массив заполнен по умолчанию
+        for (int i = 0; i < numbers.length; i++) { // Инициализация массива через цикл for, но можно и вручную присваивать значения
             numbers[i] = i * 10;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for (int i = 0; i < numbers.length; i++) { // Перебор всех элементов массива
+            System.out.println(numbers[i]); // Вывод инициализированных данных на консоль
         }
 
-        System.out.println();
+        System.out.println(); // Пустая строка, чтобы разделить вывод. Часто буду так делать, чтобы не путаться в выводе
 
-        int[] numbers1 = {1, 2, 3};
+        int[] numbers1 = {1, 2, 3}; // Создание и инициализация массива уже с готовыми данными
         for (int i = 0; i < numbers1.length; i++) {
             System.out.println(numbers1[i]);
         }
