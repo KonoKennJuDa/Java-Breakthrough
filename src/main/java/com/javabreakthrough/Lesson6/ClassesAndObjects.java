21b81a51ee7e31f4539395cac9f56232dcd46c2a
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-15 18:17:18 +0300

    Commit №15

--- src/main/java/com/javabreakthrough/Lesson6/ClassesAndObjects.java
+++ src/main/java/com/javabreakthrough/Lesson6/ClassesAndObjects.java
@@ -8,12 +8,17 @@ public class ClassesAndObjects {
         Person person = new Person(); // По созданному нами шаблону, мы создали объект и в переменные, которые были у шаблона, поместили данные
         person.name = "Роман";
         person.age = 50;
-        System.out.println("Меня зовут " + person.name + ", " + "мне " + person.age + " лет");
+        // System.out.println("Меня зовут " + person.name + ", " + "мне " + person.age + " лет");
+        person.speak(); // Мы перенесли функционал, который делается постоянно, в метод
+
+        System.out.println();
 
         Person person1 = new Person(); // Можно создавать несколько объектов из одного шаблона
         person1.name = "Михаил";
         person1.age = 20;
-        System.out.println("Меня зовут " + person1.name + ", " + "мне " + person1.age + " лет");
+        // System.out.println("Меня зовут " + person1.name + ", " + "мне " + person1.age + " лет");
+        person1.speak();
+        person1.sayHello();
     }
 }
 
@@ -24,4 +29,14 @@ class Person {
     // 2. Действия, которые он может совершать (методы)
     String name;
     int age;
+
+    void speak() {
+        for (int i = 0; i < 3; i++) { // Просто по фану прикрутили цикл, чтобы выводилось три раза
+            System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
+        }
+    }
+
+    void sayHello() { // Этому методы данные не нужны. У всех этот метод будет вызываться одинаково
+        System.out.println("Привет!");
+    }
 }
