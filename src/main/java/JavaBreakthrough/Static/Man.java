e6d043056ba2958470757b1934dadc5f420e4dcb
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-16 17:49:42 +0300

    Commit №21 static

---
+++ src/main/java/JavaBreakthrough/Static/Man.java
@@ -0,0 +1,55 @@
+package JavaBreakthrough.Static;
+
+public class Man {
+
+    // Переменные объекта. Существуют в объекте
+    private String name;
+    private int age;
+
+    // Переменные класса. Даже если нет объекта, она все равно существует
+    public static String description;
+    public static int countMan;
+
+    // Каждый раз когда создается объект, вызывается конструктор с уникальными переменными объекта и с общей переменной класса,
+    // статичной переменной, которая инкрементируется
+    public Man(String name, int age) {
+        this.name = name;
+        this.age = age;
+        countMan++; // Инкремент
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void getAllField() {
+        System.out.println( name + ", " + age + ", " + description);
+    }
+
+    public static void printAllFields () {
+        // System.out.println(name); статический метод не может работать с переменные объекта
+    }
+
+    // Метод все равно существует, даже если нет объекта
+    public static void getDescription () {
+        System.out.println(description);
+    }
+
+    // Вывод переменной класса с количеством созданных объектов. Переменная инициализируется в конструкторе
+    public void printNumberOfMan() {
+        System.out.println("Номер людей: " + countMan);
+    }
+
+}
---
+++ src/main/java/JavaBreakthrough/Static/ManApp.java
@@ -0,0 +1,36 @@
+package JavaBreakthrough.Static;
+
+public class ManApp {
+
+    public static void main(String[] args) {
+
+        // Создание объекта с переменные объекта, но у них также есть переменные класса
+        Man man = new Man("Bob", 32);
+        man.setName("Tom");
+        man.printNumberOfMan();
+
+        // Обращение к статической переменной класса, она будет общей для всех объектов
+        Man.description = "Nice";
+        Man.getDescription();
+
+        // При создании объектов, переменные объектов создаются заново, даже если они такие же, но static переменная создается один раз для всех
+
+        Man man1 = new Man("Tim", 40);
+        man1.printNumberOfMan();
+        Man man2 = new Man("Dima", 30);
+        man2.printNumberOfMan();
+        Man.description = "Good";
+        man1.getAllField();
+        man2.getAllField();
+        Man.description = "Bad";
+        man1.getAllField();
+        man2.getAllField();
+
+        System.out.println();
+
+        //Пример использования статического метода из класса Math. Math - класс коллекция со статическими методами
+        System.out.println(Math.pow(2,4));
+        System.out.println(Math.PI);
+
+    }
+}
