c0f0d95f1a992625086d46d620b1eb9f30055b53
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-16 17:11:20 +0300

    Commit №19 This

---
+++ src/main/java/JavaBreakthrough/This/Human.java
@@ -0,0 +1,28 @@
+package JavaBreakthrough.This;
+
+public class Human {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name; // this - указывает на переменную класса. Чтобы кампилятор отличал переменную класса от переменной метода
+        // this - обращение к объекту внутри класса
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void getInfo() {
+        System.out.println(name + ", " + age);
+    }
+}
---
+++ src/main/java/JavaBreakthrough/This/HumanApp.java
@@ -0,0 +1,20 @@
+package JavaBreakthrough.This;
+
+public class HumanApp {
+
+    public static void main(String[] args) {
+
+        Human human = new Human();
+        human.setName("Артем");
+        human.setAge(30);
+        human.getInfo();
+
+        System.out.println();
+
+        Human human1 = new Human();
+        human1.setName("Олег");
+        human1.setAge(40);
+        human1.getInfo();
+
+    }
+}
