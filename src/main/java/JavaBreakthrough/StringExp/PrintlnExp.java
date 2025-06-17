644351fc45679b65cca7efe473d1c8f43a222a55
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-17 22:55:08 +0300

    Commit №23 (next)

---
+++ src/main/java/JavaBreakthrough/StringExp/PrintlnExp.java
@@ -0,0 +1,12 @@
+package JavaBreakthrough.StringExp;
+
+public class PrintlnExp {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello"); // print line
+        System.out.println("Hello again"); // строка будет следующая, так как выше println
+
+        // продолжим на уроке 23-24
+    }
+}
