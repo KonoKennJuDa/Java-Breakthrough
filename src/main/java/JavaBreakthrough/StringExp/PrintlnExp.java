7349ede81f1c439f7aed40afd0cfc2640b9c682d
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-18 23:43:50 +0300

    Commit №24 final

--- src/main/java/JavaBreakthrough/StringExp/PrintlnExp.java
+++ src/main/java/JavaBreakthrough/StringExp/PrintlnExp.java
@@ -16,11 +16,25 @@ public class PrintlnExp {
 
         System.out.printf("This is a string, %s", "NICE");// printf - нужен для изменения строки. %s - означает, что будет вставлена строка
         System.out.println();
-        System.out.printf("This is a string, %d", 10); // %d - означает, что будет вставлена цифра
+        System.out.printf("This is a string, %d", 10); // %d - означает, что будет вставлена цифра/число
         System.out.println();
         System.out.printf("This is a string, %f", 11.2); // %f - означает, что будет вставлено вещественное число
         System.out.println();
         System.out.printf("%f This is a %s string, %d", 11.45, "NICE", 10); // пример
+
+        System.out.println();
+
+        System.out.printf("String %10d \n", 534); // \n - означает перенос строки, т.е как у println
+        // %10d - 10 перед d означает, что число будет 10, т.е. это выделенный размер
+        System.out.printf("String %-10d \n", 5); // Также может быть -10, минус переворачивает число, ну, как я понял, т.е. пустое место будет справа, а не слева
+        System.out.printf("String %10d \n", 123456781);
+
+        System.out.println();
+
+        System.out.printf("String %.2f \n", 12.244574575478); // %f - также можем ограничивать и округлять вещественные числа, если вставим условие перед f
+        System.out.printf("String %.2f \n", 12.24123124535); // .2 - количество цифр после запятой
+        System.out.printf("String %.2f \n", 12.2412314);
+        System.out.printf("String %.2f \n", 12.246646);
 
     }
 }
