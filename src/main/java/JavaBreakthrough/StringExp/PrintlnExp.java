da95ed2d5c7d9749e87604ab94bb2ea73a59ae8d
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-18 23:26:00 +0300

    Commit №24

--- src/main/java/JavaBreakthrough/StringExp/PrintlnExp.java
+++ src/main/java/JavaBreakthrough/StringExp/PrintlnExp.java
@@ -4,8 +4,13 @@ public class PrintlnExp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello"); // print line
-        System.out.println("Hello again"); // строка будет следующая, так как выше println
+        System.out.println("Hello"); // print line - выводит на консоль введенные параметры и переносит строку
+        System.out.println("Hello again"); // Строка будет следующая, так как выше println
+
+        System.out.println();
+
+        System.out.print("Hello, nt,"); // print - выводит на консоль введенные параметры, но не переносит строку
+        System.out.println(" he-he"); // Снова будет следом
 
         // продолжим на уроке 23-24
     }
