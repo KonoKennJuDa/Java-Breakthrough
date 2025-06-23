e1cfe0ef89f8ca7b696ee8369e387cd646bd33ef
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-23 23:52:45 +0300

    Commit №30

---
+++ src/main/java/JavaBreakthrough/Variables/PTypeExp.java
@@ -0,0 +1,18 @@
+package JavaBreakthrough.Variables;
+
+public class PTypeExp {
+
+    public static void main(String[] args) {
+
+        int age = 28;
+        short year = 2025;
+        long bigNum = 1000000000L;
+        byte smallNum = 100;
+        double price = 99.99;
+        float pi = 3.14F; // float f = 123.2; без F в конце java не поймет это double или float
+
+        System.out.println();
+
+
+    }
+}
