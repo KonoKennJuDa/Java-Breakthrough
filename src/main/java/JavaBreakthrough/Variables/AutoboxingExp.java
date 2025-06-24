a608d1ef5bfd457f69f091cf4ee31a61435f9535
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-24 23:48:45 +0300

    Commit №31a

--- src/main/java/JavaBreakthrough/Variables/AutoboxingExp.java
+++ src/main/java/JavaBreakthrough/Variables/AutoboxingExp.java
@@ -4,13 +4,21 @@ public class AutoboxingExp {
 
     public static void main(String[] args) {
 
-        int x = 1;
-        // Integer x1 = new Integer(123); так делается в редких случаях
-        Integer.parseInt(123);
+        int x = 1; // Если нужно просто число, то используется просто примитив
+
+        // Integer x1 = new Integer(123); так делается в редких случаях. Используется если нужен метод класса примитива
+        Integer.parseInt("123");
 
         // Double, Float, Long, Integer, Short, Byte, Character и Boolean - обертки
         // Обертки нужны для большего функционала, если мы хотим что-то сделать с переменной и использовать методы внутри классов каждой обертки
 
+        // Autoboxing
+        Integer x3 = 123; // Java самостоятельно упаковывает примитив в соответсвующую обертку
+        // Integer x4 = new Integer(123); - Это тоже самое, что и сверху. Java сама упаковывает
+        int y = x3; // Мы можем также присвоить его к примитиву
+        System.out.println(y);
+
+        System.out.println();
 
     }
 }
