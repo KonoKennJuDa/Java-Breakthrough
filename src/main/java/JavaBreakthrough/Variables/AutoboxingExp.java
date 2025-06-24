498508e672af85a137650c4c3269c85998999422
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-24 23:41:07 +0300

    Commit №31

---
+++ src/main/java/JavaBreakthrough/Variables/AutoboxingExp.java
@@ -0,0 +1,16 @@
+package JavaBreakthrough.Variables;
+
+public class AutoboxingExp {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+        // Integer x1 = new Integer(123); так делается в редких случаях
+        Integer.parseInt(123);
+
+        // Double, Float, Long, Integer, Short, Byte, Character и Boolean - обертки
+        // Обертки нужны для большего функционала, если мы хотим что-то сделать с переменной и использовать методы внутри классов каждой обертки
+
+
+    }
+}
