e0fda9f09bf136d1db67c6b60411a4a562cf9b39
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-25 23:54:12 +0300

    Commit №34

--- src/main/java/JavaBreakthrough/GenericsExp/GenericsExpApp.java
+++ src/main/java/JavaBreakthrough/GenericsExp/GenericsExpApp.java
@@ -1,6 +1,22 @@
 package JavaBreakthrough.GenericsExp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericsExpApp {
 
     // Generics появились в пятой Java
+
+    public static void main(String[] args) {
+
+        // Параметризация в Java
+        List animals = new ArrayList<>(); // ArrayList динамический массив
+        animals.add("Cat"); // 0
+        animals.add("Dog"); // 1
+        animals.add("Frog"); // 2
+
+        String animal = animals.get(1); // Мы клали объект класса String, а возвращаются почему-то Object
+        // Все потому, что объект класса List хранит в себе объекты класса Object, чтобы мы туда не положили
+
+    }
 }
