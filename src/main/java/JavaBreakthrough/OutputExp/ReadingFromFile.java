e4931814c9e71c0749ddb8d4a6bae2e4448a5219
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-27 21:12:22 +0300

    Commit №37

---
+++ src/main/java/JavaBreakthrough/OutputExp/ReadingFromFile.java
@@ -0,0 +1,17 @@
+package JavaBreakthrough.OutputExp;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ReadingFromFile {
+
+    public static void main(String[] args) {
+
+        File file = new File();
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Продолжу вечером
+    }
+}
