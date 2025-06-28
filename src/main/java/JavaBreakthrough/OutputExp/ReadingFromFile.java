17e0872aba5a99d37575b8a8f264043ea818d92d
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-28 23:30:16 +0300

    Commit №37 final

--- src/main/java/JavaBreakthrough/OutputExp/ReadingFromFile.java
+++ src/main/java/JavaBreakthrough/OutputExp/ReadingFromFile.java
@@ -1,7 +1,10 @@
 package JavaBreakthrough.OutputExp;
 
+import JavaBreakthrough.FinalExp.Test;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReadingFromFile {
@@ -21,13 +24,35 @@ public class ReadingFromFile {
         while (scanner.hasNextLine()) {
             System.out.println(scanner.nextLine());
         }
-        scanner.close(); // Scanner в конце работы надо закрывать
+        scanner.close(); // Scanner в конце работы надо закрывать, чтобы поток не оставался открытым
 
         System.out.println();
 
         // Другой способ чтения
 
+        String path2 = separator + "C:" + separator + "Users" + separator + "Saccaggi" + separator + "Desktop" + separator + "Text2.txt";
 
+        File file2 = new File(path2);
+
+        Scanner scanner2 = new Scanner(file2);
+        String line = scanner2.nextLine();
+        String[] numbersString = line.split(" "); // Метод split разделяет строку по пробелам. Это регулярные выражения
+        int[] numbers = new int[5];
+        int counter = 0;
+
+        for (String number : numbersString) {
+            numbers[counter++] = Integer.parseInt(number);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+
+        scanner2.close();
+
+        // Еще вариант
+
+        File file3 = new File("Test3"); // Файл лежит в корне проекта, более того, в этом же пакете
+
+        // Все это я бы еще пересмотрел и прочитал бы в нейронке задавая вопросы
 
     }
 }
