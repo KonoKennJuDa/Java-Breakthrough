6e21a8469cd9f82a7ebfdf6802b32d16e7df97b1
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-28 23:18:48 +0300

    Commit №37с

--- src/main/java/JavaBreakthrough/OutputExp/ReadingFromFile.java
+++ src/main/java/JavaBreakthrough/OutputExp/ReadingFromFile.java
@@ -13,13 +13,19 @@ public class ReadingFromFile {
         // Путь к файлу присваиваем к переменной
         String path = separator + "C:" + separator + "Users" + separator + "Saccaggi" + separator + "Desktop" + separator + "Text.txt";
 
+        // Класс File и создание объекта с нашим путем/файлом. Некая абстракция для работы далее
         File file = new File(path);
 
+        // Scanner с созданием объекта Scanner со считыванием нашего файла и цикл для вывода строчек из файла
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             System.out.println(scanner.nextLine());
         }
-        scanner.close();
+        scanner.close(); // Scanner в конце работы надо закрывать
+
+        System.out.println();
+
+        // Другой способ чтения
 
 
 
