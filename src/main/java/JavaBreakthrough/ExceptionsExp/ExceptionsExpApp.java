249d6b227ced793aa64ea356378d544e78b4c475
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-28 23:33:05 +0300

    Commit №38

---
+++ src/main/java/JavaBreakthrough/ExceptionsExp/ExceptionsExpApp.java
@@ -0,0 +1,16 @@
+package JavaBreakthrough.ExceptionsExp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ExceptionsExpApp {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File file = new File("asdf");
+        Scanner scanner = new Scanner(file); // Тут Exception
+
+        // Все это уже завтра
+    }
+}
