6b00a8921085f8f5f8a1b04bb9c1288c28e4c9d3
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-29 23:35:47 +0300

    Commit №38с

--- src/main/java/JavaBreakthrough/ExceptionsExp/ExceptionsExpApp.java
+++ src/main/java/JavaBreakthrough/ExceptionsExp/ExceptionsExpApp.java
@@ -6,12 +6,19 @@ import java.util.Scanner;
 
 public class ExceptionsExpApp {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) { //throws FileNotFoundException
 
         File file = new File("asdf");
-        Scanner scanner = new Scanner(file); // Тут Exception. Мы можем кинуть exception на метод или кинуть catch/exception
-        // Т.е. естественно, если файла нет, то будет ошибка и мы должны ее как-то обработать
 
+        // Как раз вариант с try.catch. Попробуй try, если нет, то catch ловит exception
+        try {
+            Scanner scanner = new Scanner(file); // Тут Exception. Мы можем кинуть exception на метод или кинуть catch/exception
+            // Т.е. естественно, если файла нет, то будет ошибка и мы должны ее как-то обработать
+        } catch (FileNotFoundException exception) { // Это удобно, потому что мы можем сами прописать блок с исключением, exception
+            //exception.printStackTrace(); этим методом, мы выводим ошибку на экран
+            System.out.println("Файл не найден");
+        }
+        System.out.println("После блока try catch");
 
 
     }
