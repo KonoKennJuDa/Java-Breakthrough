831b96b340df137ac7c6bc6bd5ef3191cfa2249e
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-30 23:22:33 +0300

    Commit №39 final

--- src/main/java/JavaBreakthrough/ExceptionsExp/Exception2ExpApp.java
+++ src/main/java/JavaBreakthrough/ExceptionsExp/Exception2ExpApp.java
@@ -4,19 +4,14 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Exception2ExpApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ScannerException {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             int x = Integer.parseInt((scanner.nextLine()));
 
             if (x != 0) {
-                try {
-                    throw new IOException();
-                } catch (IOException e) {
-                    System.out.println("Пользователь ввел что-то кроме нуля");
-                }
-                break;
+                throw new ScannerException("Пользователь ввел что-то кроме нуля");
             }
         }
     }
