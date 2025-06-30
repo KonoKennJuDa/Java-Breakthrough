8fa8fa2d98ec97ac0e2ce1281e0169f43a32b297
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-30 23:26:40 +0300

    Commit №39 final

--- src/main/java/JavaBreakthrough/ExceptionsExp/Exception2ExpApp.java
+++ src/main/java/JavaBreakthrough/ExceptionsExp/Exception2ExpApp.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 public class Exception2ExpApp {
     public static void main(String[] args) throws ScannerException {
 
+        // Сканер считывает введенные в консоль данные
         Scanner scanner = new Scanner(System.in);
+        // Цикл выполняется, пока int x = 0
         while (true) {
             int x = Integer.parseInt((scanner.nextLine()));
 
+            // Как только x не равен нулю, выбрасывается исключение, которые мы создали
             if (x != 0) {
-                throw new ScannerException("Пользователь ввел что-то кроме нуля");
+                throw new ScannerException("Пользователь ввел что-то кроме нуля"); // Наше исключение с параметром String
             }
         }
     }
