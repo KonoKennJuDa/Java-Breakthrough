ab15932837bc393753303aad7d8a326f98e48edf
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-06-30 23:24:16 +0300

    Commit №39 final

--- src/main/java/JavaBreakthrough/ExceptionsExp/ScannerException.java
+++ src/main/java/JavaBreakthrough/ExceptionsExp/ScannerException.java
@@ -1,8 +1,10 @@
 package JavaBreakthrough.ExceptionsExp;
 
+// Создали свой ScannerException и наследовали его от Exception
 public class ScannerException extends Exception {
 
+    // Мы переопределяем конструктор, который есть в родительском классе Exception
     public ScannerException(String description) {
-        super(description);
+        super(description); // super указывает на родителя
     }
 }
