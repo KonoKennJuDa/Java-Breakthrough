defedfb5e2d4cebf5e4d3b7859cb1ddfb0289ee9
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-07-02 23:29:05 +0300

    Commit №43

---
+++ src/main/java/JavaBreakthrough/EqualsExp/EqualsExpApp.java
@@ -0,0 +1,12 @@
+package JavaBreakthrough.EqualsExp;
+
+public class EqualsExpApp {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+        int y = 1;
+        System.out.println(x == y);
+
+    }
+}
