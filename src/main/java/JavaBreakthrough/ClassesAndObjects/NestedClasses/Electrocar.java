f77ee7899db88bcc63aa269015caaab55ceba26c
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-07-04 23:43:06 +0300

    Commit №44b

--- src/main/java/JavaBreakthrough/ClassesAndObjects/NestedClasses/Electrocar.java
+++ src/main/java/JavaBreakthrough/ClassesAndObjects/NestedClasses/Electrocar.java
@@ -4,10 +4,12 @@ public class Electrocar {
 
     private int id;
 
-    // вложенный нестатический класс
+    // Вложенный нестатический класс - относится к объекту, имеет доступ к его полям
+    // Они нужны, чтобы разграничить логику класса на отдельные части. Как тут электрокар и у него есть мотор, коробка и так далее
+    // Обычно используются с модификатором private, доступ извне не нужен. Он используется внутри самого класса
     private class Motor {
         public void startMotor() {
-            System.out.println("Motor " + id + " is starting");
+            System.out.println("Motor " + id + " is starting"); // имеет доступ к id
         }
     }
 
@@ -15,7 +17,11 @@ public class Electrocar {
         this.id = id;
     }
 
+    // Т.е. создавая метод класса Electrocar, мы внутри создаем еще и объект вложенного нестатического класса Motor и вызываем его метод
     public void start() {
+        Motor motor = new Motor(); // как раз используем класс мотор, создаем объект и через него запускам метод
+        motor.startMotor();
+
         System.out.println("Electrocar " + id + " is starting");
     }
 
