b4ec0573f973a7ca06ccc585f0b6a6cfd07cba63
Kenn'Ju <Forsaken.KennJu@gmail.com>
2025-07-05 23:57:26 +0300

    Commit №45 final

--- src/main/java/JavaBreakthrough/InputExp/WriterToFile.java
+++ src/main/java/JavaBreakthrough/InputExp/WriterToFile.java
@@ -1,9 +1,22 @@
 package JavaBreakthrough.InputExp;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class WriterToFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
+        File file = new File("testFile");
+        PrintWriter printWriter = new PrintWriter(file);
+
+        // System.out.println(System.in);
+        printWriter.println("Test row 1");
+        printWriter.println("Test row 2");
+        printWriter.println("Test row 3");
+
+        printWriter.close(); // Его обязательно надо закрыть
 
     }
 }
