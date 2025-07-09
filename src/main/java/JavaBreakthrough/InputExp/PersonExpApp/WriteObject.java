860f8cecafdf2f8b8d8644675daa42396cf9a3a2
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-07-09 23:31:46 +0300

    Commit №46 final

--- src/main/java/JavaBreakthrough/InputExp/PersonExpApp/WriteObject.java
+++ src/main/java/JavaBreakthrough/InputExp/PersonExpApp/WriteObject.java
@@ -1,6 +1,5 @@
 package JavaBreakthrough.InputExp.PersonExpApp;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
