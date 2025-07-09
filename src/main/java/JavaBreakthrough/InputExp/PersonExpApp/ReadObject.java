fe55a5b35bc7b8503e80c3bb6adb69fd20edf3b0
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-07-09 23:10:41 +0300

    Commit №46

---
+++ src/main/java/JavaBreakthrough/InputExp/PersonExpApp/ReadObject.java
@@ -0,0 +1,9 @@
+package JavaBreakthrough.InputExp.PersonExpApp;
+
+public class ReadObject {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/JavaBreakthrough/InputExp/PersonExpApp/WriteObject.java
@@ -0,0 +1,9 @@
+package JavaBreakthrough.InputExp.PersonExpApp;
+
+public class WriteObject {
+
+    public static void main(String[] args) {
+
+    }
+
+}
