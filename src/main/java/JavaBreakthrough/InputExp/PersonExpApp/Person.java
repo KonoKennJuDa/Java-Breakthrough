40d4ea1763830e54d35d032895d7f3e0db95b5fd
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-07-11 19:46:31 +0300

    Commit №48

--- src/main/java/JavaBreakthrough/InputExp/PersonExpApp/Person.java
+++ src/main/java/JavaBreakthrough/InputExp/PersonExpApp/Person.java
@@ -4,8 +4,10 @@ import java.io.Serializable;
 
 public class Person implements Serializable { // нужно имплементировать интерфейс Serializable, чтобы Java понимала, что это можно делать
 
+    // Также есть ключевое слово transient, которые запрещает сериализацию
+    // private transient int id; пример. Так как это примитивная переменная, будет 0
     private int id;
-    private String name;
+    private String name; // Переменная ссылочного типа и если запретить сериализацию, выведет Null
 
     public Person(int id, String name) {
         this.id = id;
