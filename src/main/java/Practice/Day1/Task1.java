d93e3ec88b5383a674bab2dfc75bdd0aefacba0b
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-07-15 18:20:03 +0300

    Commit task №1

---
+++ src/main/java/Practice/Day1/Task1.java
@@ -0,0 +1,15 @@
+package Practice.Day1;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int counter = 0;
+        while (counter < 10) {
+            System.out.println("Java");
+            counter++;
+        }
+
+    }
+
+}
