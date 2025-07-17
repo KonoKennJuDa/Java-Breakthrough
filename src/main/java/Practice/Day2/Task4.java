da847586d00d995e4e4d5fb29bf3c49293e7eb81
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-07-17 21:23:53 +0300

    Commit day task №2

---
+++ src/main/java/Practice/Day2/Task4.java
@@ -0,0 +1,24 @@
+package Practice.Day2;
+
+import java.util.Scanner;
+
+public class Task4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int t = a + 1;
+        while (t < b) {
+            if (t % 5 == 0 && t % 10 != 0) {
+                System.out.print(t + " ");
+            }
+            t++;
+
+        }
+
+    }
+}
