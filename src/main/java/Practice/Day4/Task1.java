5db4a3bfa7cec207fd7b717de187f5d849f0a858
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-18 21:49:45 +0300

    Commit day task №4

--- src/main/java/Practice/Day4/Task1.java
+++ src/main/java/Practice/Day4/Task1.java
@@ -1,9 +1,60 @@
 package Practice.Day4;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task1 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength = scanner.nextInt();
 
+        Random random = new Random();
+        random.nextInt(10);
+
+        int[] array = new int[arrayLength];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        System.out.println(Arrays.toString(array));
+
+        System.out.println(array.length);
+
+        int counter8 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 8)
+                counter8++;
+        }
+        System.out.println(counter8);
+
+        int counter1 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1)
+                counter1++;
+        }
+        System.out.println(counter1);
+
+        int counterEven = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0)
+                counterEven++;
+        }
+        System.out.println(counterEven);
+
+        int counterOdd = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0)
+                counterOdd++;
+        }
+        System.out.println(counterOdd);
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[1];
+        }
+        System.out.println(sum);
 
     }
 }
---
+++ src/main/java/Practice/Day4/Task2.java
@@ -0,0 +1,25 @@
+package Practice.Day4;
+
+import java.util.Random;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[100];
+
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10000);
+        }
+
+        int max = 0;
+        for (int element : array) {
+            if (element > max)
+                max = element;
+        }
+        System.out.println(max);
+
+    }
+}
