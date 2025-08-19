22ab198134024473c4f63c39e66221bd489527c3
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-19 22:52:18 +0300

    Commit day task №5

--- src/main/java/Practice/Day5/Task1.java
+++ src/main/java/Practice/Day5/Task1.java
@@ -4,6 +4,44 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        Car car = new Car();
+        car.setColor("black");
+        car.setYear(2000);
+        car.setModel("AUDI");
 
+        System.out.println("Our car: " + car.getYear() + " year " + car.getModel() + " model " + car.getColor() + " color");
     }
+
+}
+
+class Car {
+
+    private int year;
+    private String model;
+    private String color;
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }
