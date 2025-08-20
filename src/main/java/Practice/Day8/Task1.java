26b2ded164f2d44ae98128e336afc0575fc8415b
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-20 23:37:10 +0300

    Commit day task №8

--- src/main/java/Practice/Day8/Task1.java
+++ src/main/java/Practice/Day8/Task1.java
@@ -4,11 +4,11 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        //Плохой вариант конкатенации, String иммутабельная. Каждый раз будет создаваться новая строка
         String numbers = "";
 
         long before = System.currentTimeMillis();
 
-        //Плохой вариант конкатенации, String иммутабельная. Каждый раз будет создаваться новая строка
         for (int i = 0; i <= 20000; i++) {
             numbers += +i + " ";
         }
@@ -18,7 +18,7 @@ public class Task1 {
         System.out.println("Время выполнения цикла с классом String: " + (after - before));
         System.out.println(numbers);
 
-
+        //Хороший вариант конкатенации, StringBuilder внутри имеет массив чаров или байтов
         StringBuilder stringBuilder = new StringBuilder("");
 
         before = System.currentTimeMillis();
