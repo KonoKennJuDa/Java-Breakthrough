a956347304dc647ecc0db3a994e61e538a4ebab9
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-21 23:27:33 +0300

    Commit day task №9

--- src/main/java/Practice/Day9/Task1.java
+++ src/main/java/Practice/Day9/Task1.java
@@ -5,6 +5,40 @@ public class Task1 {
     public static void main(String[] args) {
 
 
-
     }
+}
+
+class Human {
+    private String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Это человек с именем" + name);
+    }
+
+}
+
+class Student extends Human {
+    private String groupName;
+
+    public Student(String name, String groupName) {
+        super(name);
+        this.groupName = groupName;
+    }
+
+}
+
+class Teacher extends Human {
+
 }
