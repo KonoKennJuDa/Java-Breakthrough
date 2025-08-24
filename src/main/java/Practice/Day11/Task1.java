ba846454427b7edb4b88e91892075226d978886c
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-24 23:27:50 +0300

    Commit day task №11

--- src/main/java/Practice/Day11/Task1.java
+++ src/main/java/Practice/Day11/Task1.java
@@ -7,3 +7,26 @@ public class Task1 {
 
     }
 }
+
+class Warehouse {
+    private int balance;
+    private int countOrders;
+
+    @Override
+    public String toString() {
+        return "Warehouse{" +
+                "balance=" + balance +
+                ", countOrders=" + countOrders +
+                '}';
+    }
+}
+
+class Picker {
+    private int salary;
+
+}
+
+class Courier {
+    private int salary;
+
+}
