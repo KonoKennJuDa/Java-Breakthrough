e2352b8c7f5ce65fc1ab40834b70f13f9073ab7b
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-24 23:39:30 +0300

    Commit day task №11

--- src/main/java/Practice/Day11/Task1.java
+++ src/main/java/Practice/Day11/Task1.java
@@ -4,6 +4,23 @@ public class Task1 {
 
     public static void main(String[] args) {
 
+        Warehouse warehouse = new Warehouse();
+        Picker picker = new Picker(warehouse);
+        Courier courier = new Courier(warehouse);
+
+        System.out.println(warehouse + " " + picker + " " + courier);
+
+        picker.doWork();
+        courier.doWork();
+        System.out.println(warehouse + " " + picker + " " + courier);
+
+        for (int i = 1; i <= 1500; i++) {
+            picker.doWork();
+            courier.doWork();
+        }
+        picker.bonus();
+        courier.bonus();
+        System.out.println(warehouse + " " + picker + " " + courier);
 
     }
 }
@@ -11,6 +28,22 @@ public class Task1 {
 class Warehouse {
     private int balance;
     private int countOrders;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getCountOrders() {
+        return countOrders;
+    }
+
+    public void setCountOrders(int countOrders) {
+        this.countOrders = countOrders;
+    }
 
     @Override
     public String toString() {
@@ -21,12 +54,66 @@ class Warehouse {
     }
 }
 
-class Picker {
+class Picker implements Worker {
     private int salary;
+    private Warehouse warehouse;
 
+    public Picker(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public void doWork() {
+        salary += 100;
+        warehouse.setCountOrders(warehouse.getCountOrders() + 1);
+    }
+
+    @Override
+    public void bonus() {
+        if (warehouse.getCountOrders() > 1500) {
+            salary *= 3;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Picker{" +
+                "salary=" + salary +
+                '}';
+    }
 }
 
-class Courier {
+class Courier implements Worker {
     private int salary;
+    private Warehouse warehouse;
 
+    public Courier(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+
+    @Override
+    public void doWork() {
+        salary += 80;
+        warehouse.setBalance(warehouse.getBalance() + 1000);
+    }
+
+    @Override
+    public void bonus() {
+        if (warehouse.getBalance() > 1000000) {
+            salary *= 2;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Courier{" +
+                "salary=" + salary + '}';
+    }
+}
+
+interface Worker {
+    void doWork();
+
+    void bonus();
 }
