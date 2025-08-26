076599507547eb3ec33e5f247e51297936bdaff3
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-27 00:03:38 +0300

    Commit day task №12

---
+++ src/main/java/Practice/Day12/Task3.java
@@ -0,0 +1,78 @@
+package Practice.Day12;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        MusicBand band1 = new MusicBand("XXX", 1950);
+        MusicBand band2 = new MusicBand("YYY", 1920);
+        MusicBand band3 = new MusicBand("GGG", 1930);
+        MusicBand band4 = new MusicBand("FFF", 1910);
+        MusicBand band5 = new MusicBand("SSS", 2010);
+        MusicBand band6 = new MusicBand("ZZZ", 2015);
+
+
+        List<MusicBand> musicBands = new ArrayList<>();
+        musicBands.add(band1);
+        musicBands.add(band2);
+        musicBands.add(band3);
+        musicBands.add(band4);
+        musicBands.add(band5);
+        musicBands.add(band6);
+
+        System.out.println(musicBands);
+
+        Collections.shuffle(musicBands);
+
+        System.out.println(musicBands);
+
+        List<MusicBand> musicBandsAfter2000 = new ArrayList<>();
+
+        for (MusicBand band : musicBands) {
+            if (band.getYear() > 2000) {
+                musicBandsAfter2000.add(band);
+            }
+        }
+
+        System.out.println(musicBandsAfter2000);
+
+    }
+}
+
+class MusicBand {
+    private String name;
+    private int year;
+
+    public MusicBand(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "MusicBand{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ src/main/java/Practice/Day12/Task4.java
@@ -0,0 +1,4 @@
+package Practice.Day12;
+
+public class Task4 {
+}
