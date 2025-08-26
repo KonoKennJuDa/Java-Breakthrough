3e56ed05295c20ad536da0a2ac108cfbd4788853
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-08-26 23:51:40 +0300

    Commit day task №12

--- src/main/java/Practice/Day12/Task1.java
+++ src/main/java/Practice/Day12/Task1.java
@@ -1,9 +1,22 @@
 package Practice.Day12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
 
     public static void main(String[] args) {
 
+
+        List<String> list = new ArrayList<>();
+
+        list.add("Audi");
+        list.add("BMW");
+        list.add("Lada");
+        list.add("UAZ");
+        list.add("Toyota");
+
+        System.out.println(list);
 
     }
 }
