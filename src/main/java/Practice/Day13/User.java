84a25a4ddb1244b92d0a32766c946350b2cf36ea
Kenn'Ju <Forsaken.kennju@gmail.com>
2025-09-02 00:09:26 +0300

    Commit day task №13

---
+++ src/main/java/Practice/Day13/User.java
@@ -0,0 +1,47 @@
+package Practice.Day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscriptions;
+
+    public User(String username) {
+        this.username = username;
+        this.subscriptions = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(List<User> subscriptions) {
+        this.subscriptions = subscriptions;
+    }
+
+    public void subscribe(User user) {
+        this.subscriptions.add(user);
+    }
+
+    public boolean isSubscribed(User user) {
+        for (User currentUser : subscriptions) {
+            if (currentUser.getUsername().equals(user.getUsername())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isFriend(User user) {
+        return false;
+    }
+}
